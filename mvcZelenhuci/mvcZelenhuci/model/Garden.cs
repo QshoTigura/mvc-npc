4ef8f2c1d667f03ff44aee0109bccae63e2727f5
QshoTigura <q.mehandjiiski2019@pmg-pernik.com>
2023-02-13 10:33:29 +0200

    model

---
+++ mvcZelenhuci/mvcZelenhuci/model/Garden.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mvcZelenhuci.model
+{
+    public class Garden
+    {
+        public Garden(decimal vefiPrice, decimal fruitsPrice, int vegiKG, int fruitsKG)
+        {
+            VefiPrice = vefiPrice;
+            FruitsPrice = fruitsPrice;
+            VegiKG = vegiKG;
+            FruitsKG = fruitsKG;
+        }
+
+        public decimal VefiPrice { get; set; }
+        public decimal FruitsPrice { get; set; }
+        public int VegiKG { get; set; }
+        public int FruitsKG { get; set; }
+        public decimal CalculateSum()
+        {
+            return 1.94m * (this.FruitsKG * this.FruitsPrice + VegiKG * VefiPrice);
+        }
+    }
+}
