5ab5db7fa283974779ac82bf547b7deee3899d50
QshoTigura <q.mehandjiiski2019@pmg-pernik.com>
2023-02-13 11:23:02 +0200

    add view

---
+++ mvcZelenhuci/mvcZelenhuci/view/Display.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mvcZelenhuci.view
+{
+    public class Display
+    {
+        public decimal VefiPrice { get; set; }
+        public decimal FruitsPrice { get; set; }
+        public int VegiKG { get; set; }
+        public int FruitsKG { get; set; }
+        public decimal Total { get; set; }
+
+        public void GetValues()
+        {
+            this.VefiPrice = decimal.Parse(Console.ReadLine());
+            this.FruitsPrice = decimal.Parse(Console.ReadLine());
+            this.VegiKG = int.Parse(Console.ReadLine());
+            this.FruitsKG = int.Parse(Console.ReadLine());
+        }
+
+        public void Showresut()
+        {
+            Console.WriteLine(this.Total);
+        }
+    }
+}
